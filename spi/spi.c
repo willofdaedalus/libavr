25fbf22a229b34f7a73e77715e1a383c07a88e8c
willofdaedalus <manny.is.daedalus@pm.me>
2025-09-08 19:10:10 +0000

    lookup table for spi config

--- spi/spi.c
+++ spi/spi.c
@@ -1,114 +1,133 @@
 #include "spi.h"
 #include <avr/io.h>
 
-// configures spi mode as slave device unless MASTER is defined
-uint8_t spi_init(struct spi_s *config)
+#define SPR0_BIT (1 << SPR0)
+#define SPR1_BIT (1 << SPR1)
+#define SPR0_SPR1 (1 << SPR0 | 1 << SPR1)
+#define CPHA_BIT (1 << CPHA)
+#define CPOL_BIT (1 << CPOL)
+#define CPOL_CPHA (1 << CPOL | 1 << CPHA)
+
+// instead of building SPCR bits at runtime, we precompute
+// divider/mode combinations in spcr_table[] for speed and clarity.
+// MSTR, DORD, and SPI2X are applied separately.
+
+// each entry is structured like so
+// {mode 0, mode 1, mode 2, mode 3}
+static const uint8_t spcr_table[5][4] = {
+	// div4 and div2
+	{0, CPOL_BIT, CPHA_BIT, CPOL_CPHA},
+
+	// div16 and div8
+	{SPR0_BIT,
+	 SPR0_BIT | CPOL_BIT,
+	 SPR0_BIT | CPHA_BIT,
+	 SPR0_BIT | CPOL_CPHA},
+
+	// div32 and div64 (normal)
+	{SPR1_BIT,
+	 SPR1_BIT | CPOL_BIT,
+	 SPR1_BIT | CPHA_BIT,
+	 SPR1_BIT | CPOL_BIT | CPHA_BIT},
+
+	// div128 and div64 (fast version)
+	{SPR0_SPR1,
+	 SPR0_SPR1 | CPOL_BIT,
+	 SPR0_SPR1 | CPHA_BIT,
+	 SPR0_SPR1 | CPHA_BIT | CPOL_BIT},
+};
+
+// ensures the speeds match their clock divisions
+// returns -1 on mismatch or 0 on legal match
+uint8_t validate_matches(uint8_t speed, uint8_t sck_div)
 {
-	if (!config)
-		return -1;
-
-	if (config->master)
-		SPCR |= _BV(MSTR);
-
-	if (config->spie)
-		SPCR |= _BV(SPIE);
-
-	// enable 2x speed based on what the user set
-	if (config->speed_mode) {
-		switch (config->c_div) {
+	if (speed) {
+		// 2x mode only supports: DIV2, DIV8, DIV32, DIV64
+		switch (sck_div) {
 		case SPI_SCK_DIV2:
 		case SPI_SCK_DIV8:
 		case SPI_SCK_DIV32:
 		case SPI_SCK_DIV64:
-			SPCR |= _BV(SPI2X);
-			break;
-
+			break; // Valid combinations
 		default:
-			// reset the SPCR on error
-			SPCR = 0x0;
-			return ESPI_SPEED_MISMATCH;
+			return -SPI_ERR_SPEED_MISMATCH;
 		}
 	} else {
-		switch (config->c_div) {
+		// Normal mode only supports: DIV4, DIV16, DIV64, DIV128
+		switch (sck_div) {
 		case SPI_SCK_DIV4:
 		case SPI_SCK_DIV16:
 		case SPI_SCK_DIV64:
 		case SPI_SCK_DIV128:
-			SPCR &= ~_BV(SPI2X);
-			break;
-
+			break; // Valid combinations
 		default:
-			SPCR = 0x0;
-			return ESPI_SPEED_MISMATCH;
+			return -SPI_ERR_SPEED_MISMATCH;
 		}
 	}
 
-	// intentional resetting in case user reinitializes their spi
-	// mode again
-	switch (config->c_div) {
+	return SPI_OK;
+}
+
+// configures spi mode based on user's cfg
+// returns 0 for successful or non-zero on failure
+uint8_t spi_init(struct spi_config_s *cfg)
+{
+	uint8_t div_idx, res;
+	if (!cfg)
+		return -1;
+
+	if ((res = validate_matches(cfg->speed_mode, cfg->sck_div)) != SPI_OK)
+		return res;
+
+	switch (cfg->sck_div) {
 	case SPI_SCK_DIV2:
 	case SPI_SCK_DIV4:
-		SPCR &= ~_BV(SPR1);
-		SPCR &= ~_BV(SPR0);
+		div_idx = 0;
 		break;
 
-	case SPI_SCK_DIV16:
 	case SPI_SCK_DIV8:
-		SPCR &= ~_BV(SPR1);
-		SPCR |= _BV(SPR0);
+	case SPI_SCK_DIV16:
+		div_idx = 1;
 		break;
 
 	case SPI_SCK_DIV32:
-		SPCR |= _BV(SPR1);
-		SPCR &= ~_BV(SPR0);
+		div_idx = 2;
 		break;
 
 	case SPI_SCK_DIV64:
-		if (config->speed_mode) {
-			SPCR |= _BV(SPR1) | _BV(SPR0);
-		} else {
-			SPCR |= _BV(SPR1);
-			SPCR &= ~_BV(SPR0);
-		}
+		div_idx = 3;
 		break;
 
 	case SPI_SCK_DIV128:
-		SPCR |= _BV(SPR1) | _BV(SPR0);
+		div_idx = 3;
 		break;
+	default:
+		return -SPI_ERR_SPEED_MISMATCH;
 	}
 
-	// cpol cpha
-	switch (config->mode) {
-	case SPI_MODE_0: // 0 0
-		SPCR &= ~_BV(CPOL);
-		SPCR &= ~_BV(CPHA);
-		break;
+	SPCR = spcr_table[div_idx][cfg->mode];
 
-	case SPI_MODE_1: // 0 1
-		SPCR &= ~_BV(CPOL);
-		SPCR |= _BV(CPHA);
-		break;
+	if (cfg->speed_mode)
+		SPSR = _BV(SPI2X);
 
-	case SPI_MODE_2: // 1 0
-		SPCR |= _BV(CPOL);
-		SPCR &= ~_BV(CPHA);
-		break;
+	if (cfg->master)
+		SPCR |= _BV(MSTR);
 
-	case SPI_MODE_3: // 1 1
-		SPCR |= _BV(CPOL);
-		SPCR |= _BV(CPHA);
-		break;
-	}
-
-	if (config->lsb) {
+	if (cfg->lsb)
 		SPCR |= _BV(DORD);
-	} else {
-		SPCR &= ~_BV(DORD);
-	}
 
-
-	// finally enable spi mode
-	SPCR |= _BV(SPE);
+	SPCR |= _BV(SPE) | _BV(SPIE);
 
 	return SPI_OK;
+}
+
+// sends a byte, reads and returns another byte from the other peripheral
+uint8_t spi_send(uint8_t data)
+// configures spi mode based on user's cfg
+{
+	SPDR = data;
+	loop_until_bit_is_set(SPSR, SPIF);
+
+	// below clears the SPIF flag in the SPI status register
+	return SPDR;
 }
--- spi/spi.h
+++ spi/spi.h
@@ -13,25 +13,23 @@
 
 #define SPI_OK (0)
 
-enum SPI_Error {
-	ESPI_SPEED_MISMATCH = 1,
-};
+#define SPI_ERR_SPEED_MISMATCH (0x01)
 
 #define SPI_MODE_0 (0)
 #define SPI_MODE_1 (1)
 #define SPI_MODE_2 (2)
 #define SPI_MODE_3 (3)
 
-struct spi_s {
-	uint8_t spie;
-	uint8_t c_div;
+struct spi_config_s {
+	uint8_t sck_div;
 	uint8_t master;
 	uint8_t mode;
 	uint8_t speed_mode;
 	uint8_t lsb;
 };
 
-uint8_t spi_init(struct spi_s *config);
+uint8_t spi_init(struct spi_config_s *config);
+uint8_t spi_send(uint8_t data);
 
 
 #endif //
