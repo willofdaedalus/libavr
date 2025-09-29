1b2f38ba8a83c1fe53364308b41dce760fde7658
willofdaedalus <manny.is.daedalus@pm.me>
2025-09-29 18:09:14 +0000

    i2c library mostly complete

--- src/i2c/i2c.c
+++ src/i2c/i2c.c
@@ -1,19 +1,27 @@
 #include "i2c.h"
 
+// initialize the i2c interface
 void i2c_init(void)
 {
-	TWCR = _BV(TWEN);
+	// set scl frequency
+	// FOR 100KHZ I2C WITH F_CPU = 16MHz:
+	// 100000 = 16000000 / (16 + 2 * TWBR * 1)
+	// twbr = 72
+
+	TWSR = 0; // Prescaler = 1 (bits 0-1 are prescaler, clear them)
+	TWBR = ((F_CPU / 100000UL) - 16) / 2; // For 100kHz
+
+	TWCR = _BV(TWEN); // Enable TWI
 }
 
 // send a start condition
-void i2c_start_tx(void)
+void i2c_start(void)
 {
 	// send a start condition
 	TWCR = _BV(TWINT) | _BV(TWSTA) | _BV(TWEN);
 
 	// wait for the TWINT flag to be set
-	while (!(TWCR & _BV(TWINT)))
-		;
+	i2c_wait_twint();
 }
 
 // send a stop condition
@@ -22,7 +30,54 @@ void i2c_stop(void)
 	TWCR = _BV(TWINT) | _BV(TWSTO) | _BV(TWEN);
 }
 
+uint8_t i2c_read_ack(void)
+{
+	TWCR = _BV(TWINT) | _BV(TWEN) | _BV(TWEA);
+	i2c_wait_twint();
 
-// void i2c_master_tx(uint8_t data)
-// {
-// }
+	return TWDR;
+}
+
+uint8_t i2c_read_nack(void)
+{
+	TWCR = _BV(TWINT) | _BV(TWEN);
+	i2c_wait_twint();
+
+	return TWDR;
+}
+
+// i2c_wait_twint waits on the TWINT flag to be set;
+// useful for checking if an ack has been received or data has been transmitted
+void i2c_wait_twint(void)
+{
+	while (!(TWCR & _BV(TWINT)))
+		;
+}
+
+uint8_t i2c_sla_tx(uint8_t sla, uint8_t rw)
+{
+	// i2c slave addresses are 7-bits; any longer and that's an issue
+	if (sla > 0x7f || sla == 0)
+		return I2C_ERR_BAD_SLA;
+
+	if (rw > 1)
+		return I2C_ERR_BAD_RW;
+
+	// shift the bits to the left by 1 and append the rw
+	sla = (sla << 1) | rw;
+	TWDR = sla;
+	TWCR = _BV(TWINT) | _BV(TWEN);
+
+	i2c_wait_twint();
+
+	return I2C_OK;
+}
+
+// send data to the peripheral in address
+void i2c_send_data(uint8_t data)
+{
+	TWDR = data;
+	TWCR = _BV(TWINT) | _BV(TWEN);
+
+	i2c_wait_twint();
+}
--- src/i2c/i2c.h
+++ src/i2c/i2c.h
@@ -4,11 +4,31 @@
 #include <avr/io.h>
 
 // MASTER TRANSMITTER MODE CODES
-#define I2C_START (0x08)      // start condition has been sent
-#define I2C_REP_START (0x10)  // repeated start has been sent
-#define I2C_MT_SLA_ACK (0x18) // sla+w tx; ack received
+#define I2C_START (0x08)	    // start condition has been sent
+#define I2C_REP_START (0x10)	    // repeated start has been sent
+#define I2C_MT_SLA_ACK (0x18)	    // sla+w tx; ack received
+#define I2C_MT_SLA_NACK (0x20)	    // sla+w tx; not ack recieved
+#define I2C_MT_DATA_ACK (0x28)	    // data tx; ack received
+#define I2C_MT_DATA_NACK (0x30)	    // data tx; not ack received
+#define I2C_ARBITRATION_LOST (0x38) // arbitration lost in sla+w
 
+// MASTER RECEIVE MODE CODES
+#define I2C_MR_SLA_ACK (0x40)	// sla+r rx; ack received
+#define I2C_MR_SLA_NACK (0x48)	// sla+r rx; not ack received
+#define I2C_MR_DATA_ACK (0x50)	// data rx; ack received
+#define I2C_MR_DATA_NACK (0x50) // data rx; not ack received
+
+#define I2C_SLA_W (0)
+#define I2C_SLA_R (1)
+
+#define I2C_OK (0)
+#define I2C_ERR_BAD_SLA (0xd0)
+#define I2C_ERR_BAD_RW (0xd1)
 
 void i2c_init(void);
+void i2c_start(void);
+void i2c_stop(void);
+uint8_t i2c_tx(uint8_t sla, uint8_t rw);
+void i2c_wait_twint(void);
 
 #endif // _I2C_H
